1142c33c89cdf4f69179dc4e6593427f4c551e77
wpeng <wpeng@sdsu.edu>
2025-02-14 15:06:36 -0800

    Revert "Lab4 Code"
    This reverts commit a279ffa6850035a0ac96045af0237c2563a256fc.

--- Lab4/src/main/java/TestMain.java
+++
@@ -1,8 +0,0 @@
-/**
- * Nothing much here, look for TODOs in your code (Total 5 places)
- */
-public class TestMain {
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-	}
-}
