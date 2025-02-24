3e92498ce21920103ecc49b90e9700d073043d9c
wpeng <wpeng@sdsu.edu>
2025-02-24 10:06:29 -0800

    Lab5 Code

--- Lab5/src/main/java/edu/sdsu/cs160l/university/course/CourseFactory.java
+++ Lab5/src/main/java/edu/sdsu/cs160l/university/course/CourseFactory.java
@@ -9,8 +9,6 @@ public class CourseFactory {
     public static Course getCourse(String courseName){
         switch (courseName){
             //TODO: add a case for cs150 course
-            case "CS150":
-                return new CS150();
             case "CS160":
                 return new CS160();
             case "CS210":
